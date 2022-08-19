808a67c16b5a3c007caf62b8e65fb48f68c0ae45
Smail <44433524+Smail@users.noreply.github.com>
2022-08-20 00:15:39 +0200

    feat: add calendar view

--- src/views/CalendarView.vue
+++ src/views/CalendarView.vue
@@ -1,2 +1,30 @@
 <template>
+  <div class="calendar">
+    <h1>Calendar</h1>
+    <calendar class="calendar-component"></calendar>
+  </div>
 </template>
+
+<style lang="scss">
+.calendar {
+  display: flex;
+  flex-direction: column;
+
+  h1 {
+    align-self: flex-start;
+  }
+
+  .calendar-component {
+    flex: 1;
+  }
+}
+</style>
+
+<script>
+import Calendar from "@/components/Calendar.vue";
+
+export default {
+  name: 'CalendarView',
+  components: { Calendar }
+}
+</script>
