8a7324ae73eaba883eb7600293eb269e1a98742c
Smail <44433524+Smail@users.noreply.github.com>
2022-08-20 00:13:49 +0200

    feat: add toggle button component

---
+++ src/components/ToggleButton.vue
@@ -0,0 +1,42 @@
+<template>
+  <button class="material-symbols-outlined toggle-button" :class="{ 'active': isActive }">
+    {{ isActive ? 'toggle_on' : 'toggle_off' }}
+  </button>
+</template>
+
+<style lang="scss">
+.toggle-button {
+  padding: 0;
+  margin: 0;
+  cursor: pointer;
+  background-color: transparent;
+  border: none;
+  outline: none;
+  transition: all 500ms;
+  // If not active set color to red
+  color: rgb(255, 59, 48);
+
+  &.active {
+    color: rgb(52, 199, 89);
+  }
+
+  &.material-symbols-outlined {
+    font-variation-settings: 'FILL' 1,
+      'wght' 400,
+      'GRAD' 200,
+      'opsz' 20;
+  }
+}
+</style>
+
+<script>
+export default {
+  name: 'ToggleButton',
+  props: {
+    isActive: {
+      type: Boolean,
+      required: true,
+    }
+  }
+}
+</script>
