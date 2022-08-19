df1ff77ce573b82c5a4dc541d6d6a9b5fca6599c
Smail <44433524+Smail@users.noreply.github.com>
2022-08-20 00:12:06 +0200

    feat: add signup view

--- src/views/SignUpView.vue
+++ src/views/SignUpView.vue
@@ -1,2 +1,30 @@
 <template>
+  <div class="signup">
+    <div class="signup-form-wrapper">
+      <sign-up-form title="Welcome back!"></sign-up-form>
+    </div>
+  </div>
 </template>
+
+<script>
+import SignUpForm from '@/components/SignUpForm.vue'
+
+export default {
+  name: "SignUpView",
+  components: { SignUpForm },
+}
+</script>
+
+<style lang="scss">
+.signup {
+  display: flex;
+  // Center sign up component
+  flex-direction: column;
+  justify-content: center;
+}
+
+.signup-form-wrapper {
+  display: flex;
+  justify-content: center;
+}
+</style>
