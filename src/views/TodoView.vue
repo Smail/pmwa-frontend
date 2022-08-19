2ddecd0b5b4572cd4b08c64877e42d142ec7a6bb
Smail <44433524+Smail@users.noreply.github.com>
2022-08-20 00:14:28 +0200

    feat: add todo view

--- src/views/TodoView.vue
+++ src/views/TodoView.vue
@@ -1,2 +1,26 @@
 <template>
+  <div class="todo">
+    <h1>Tasks</h1>
+    <task-list></task-list>
+  </div>
 </template>
+
+<style lang="scss">
+.todo {
+  display: flex;
+  flex-direction: column;
+  margin: 1rem 0;
+
+  h1 {
+    align-self: flex-start;
+  }
+}
+</style>
+
+<script>
+import TaskList from "@/components/TaskList.vue";
+export default {
+    name: "TodoView",
+    components: { TaskList }
+}
+</script>
