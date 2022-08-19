608ce4f9ed731063ee2ba47674db4977d54b1a13
Smail <44433524+Smail@users.noreply.github.com>
2022-08-20 00:12:27 +0200

    feat: add sign in view

--- src/views/SignInView.vue
+++ src/views/SignInView.vue
@@ -1,2 +1,30 @@
 <template>
+  <div class="login">
+    <div class="login-form-wrapper">
+      <sign-in-form title="Welcome back!"></sign-in-form>
+    </div>
+  </div>
 </template>
+
+<script>
+import SignInForm from '@/components/SignInForm.vue'
+
+export default {
+  name: "SignInView",
+  components: { SignInForm },
+}
+</script>
+
+<style lang="scss">
+.login {
+  display: flex;
+  // Center sign in component
+  flex-direction: column;
+  justify-content: center;
+}
+
+.login-form-wrapper {
+  display: flex;
+  justify-content: center;
+}
+</style>
