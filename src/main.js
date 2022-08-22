375e12e190f942e09faffc78143943a4dcfc2d56
Smail <44433524+Smail@users.noreply.github.com>
2022-08-22 06:17:28 +0200

    feat: add axios interceptor for 401 unauthorized and force re-login

--- src/main.js
+++ src/main.js
@@ -7,6 +7,20 @@ import VueAxios from 'vue-axios'
 
 axios.defaults.baseURL = `${ process.env.VUE_APP_API_ENDPOINT }`;
 
+axios.interceptors.response.use(r => r, function (error) {
+  // Any status codes that falls outside the range of 2xx cause this function to trigger
+  // Do something with response error
+  if (error.response.status === 401) {
+    // If an unauthorized status code appears, then
+    // the user credentials are no longer valid if there even are any.
+    // Remove tokens from storage to force a re-login.
+    localStorage.removeItem('accessToken');
+    localStorage.removeItem('refreshToken');
+  }
+
+  return Promise.reject(error);
+});
+
 createApp(App)
   .use(store)
   .use(router)
