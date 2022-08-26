fe79c31a632706d97839bf4c0b81ed96388084fd
Smail <44433524+Smail@users.noreply.github.com>
2022-08-26 07:16:38 +0200

    fix: user not getting logged out when http code forbidden comes in

--- src/main.js
+++ src/main.js
@@ -33,11 +33,8 @@ axios.interceptors.response.use(r => r, function (error) {
   // Any status codes that falls outside the range of 2xx cause this function to trigger
   // Do something with response error
   if (error.response.status === 401) {
-    // If an unauthorized status code appears, then
-    // the user credentials are no longer valid if there even are any.
-    // Remove tokens from storage to force a re-login.
-    localStorage.removeItem('accessToken');
-    localStorage.removeItem('refreshToken');
+    // 401 means forbidden access and this may happen when all tokens get invalidated.
+    store.dispatch('logOut');
   }
 
   return Promise.reject(error);
