e71675cd1480569190651175299a024c0c910ff4
Smail <44433524+Smail@users.noreply.github.com>
2022-08-27 08:06:18 +0200

    feat(tasks): move create new task segment to the top

--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -1,10 +1,10 @@
 <template>
   <ul class="task-list">
-    <li v-for="(task, i) in $store.state.tasks" :key="i">
-      <task class="task-wrapper" :task="task" @refresh-tasks="loadTasks"></task>
-    </li>
     <li>
       <create-task class="new-task-wrapper" @refresh-tasks="loadTasks"></create-task>
+    </li>
+    <li v-for="(task, i) in $store.state.tasks.slice().reverse()" :key="i">
+      <task :task="task" class="task-wrapper" @refresh-tasks="loadTasks"></task>
     </li>
   </ul>
 </template>
