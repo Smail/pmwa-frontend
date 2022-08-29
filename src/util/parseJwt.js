057e45130f87a6cc108f8a4a0b42aaf6d2f9a3e5
Smail <44433524+Smail@users.noreply.github.com>
2022-08-29 15:08:39 +0200

    refactor: throw error if argument is falsy

--- src/util/parseJwt.js
+++ src/util/parseJwt.js
@@ -1,4 +1,5 @@
 export function parseJwt(token) {
+  if (!token) throw new Error('Invalid argument');
   const base64Url = token.split('.')[1];
   const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
   const jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function (c) {
