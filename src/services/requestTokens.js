892c5b22b9ff4ec363fce2002ae2bbe9e7ee779e
Smail <44433524+Smail@users.noreply.github.com>
2022-08-29 13:02:23 +0200

    refactor: extract token request and jwt functions

---
+++ src/services/requestTokens.js
@@ -0,0 +1,40 @@
+import axios from "axios";
+import { parseJwt } from "@/util/parseJwt";
+import { hasTokenExpired } from "@/util/hasTokenExpired";
+
+export async function requestTokens({ refreshToken, username, password }) {
+  console.debug('Requesting new token pair');
+
+  // Check if more than one request method is used.
+  if (refreshToken && (username || password)) {
+    throw new Error('Too many possible routes. Either request tokens via credentials or a refresh token');
+  }
+
+  if (refreshToken) {
+    return requestTokensViaRefreshToken(refreshToken);
+  } else if (username && password) {
+    return requestTokensViaCredentials(username, password);
+  } else {
+    throw new Error('No credential methods applied. Please pass a refresh token or user credentials.');
+  }
+}
+
+async function requestTokensViaRefreshToken(refreshToken) {
+  if (refreshToken == null) throw new Error('No refresh token found in local storage');
+  if (hasTokenExpired(refreshToken)) throw new Error('Refresh token has expired');
+  const refreshTokenPayload = parseJwt(refreshToken);
+  const response = await axios.post('auth/refresh-token', {
+    username: refreshTokenPayload.username,
+    refreshToken: refreshToken,
+  });
+
+  return { accessToken: response.data.accessToken, refreshToken: response.data.refreshToken };
+}
+
+async function requestTokensViaCredentials(username, password) {
+  if (!username) throw new Error('Invalid argument. Username is falsy.');
+  if (!password) throw new Error('Invalid arguments. Password is falsy.');
+
+  const response = await axios.post(`${ process.env.VUE_APP_API_ENDPOINT }/auth/signin`, { username, password });
+  return { accessToken: response.data.accessToken, refreshToken: response.data.refreshToken };
+}
--- src/store/index.js
+++ src/store/index.js
@@ -1,34 +1,8 @@
 import { createStore } from 'vuex'
 import axios from "axios";
-
-function parseJwt(token) {
-  const base64Url = token.split('.')[1];
-  const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-  const jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function (c) {
-    return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-  }).join(''));
-
-  return JSON.parse(jsonPayload);
-}
-
-function hasTokenExpired(token) {
-  return Date.now() < token.exp * 1000;
-}
-
-async function requestNewTokenPair(refreshToken) {
-  console.debug('Requesting new token pair');
-  if (refreshToken == null) throw new Error('No refresh token found in local storage');
-  if (hasTokenExpired(refreshToken)) throw new Error('Refresh token has expired');
-  const refreshTokenPayload = parseJwt(refreshToken);
-  const response = await axios.post('auth/refresh-token', {
-    username: refreshTokenPayload.username,
-    refreshToken: refreshToken,
-  });
-  return {
-    newAccessToken: response.data.accessToken,
-    newRefreshToken: response.data.refreshToken
-  };
-}
+import { requestTokens } from "@/services/requestTokens";
+import { parseJwt } from "@/util/parseJwt";
+import { hasTokenExpired } from "@/util/hasTokenExpired";
 
 export default createStore({
   state: {
@@ -74,7 +48,7 @@ export default createStore({
         // Remove old refresh token in case the request fails
         localStorage.removeItem('refreshToken');
         // Request new token when no access token is available and the refresh token is valid.
-        const { newAccessToken, newRefreshToken } = await requestNewTokenPair(refreshToken);
+        const { newAccessToken, newRefreshToken } = await requestTokens({ refreshToken });
 
         localStorage.setItem('accessToken', (accessToken = newAccessToken));
         localStorage.setItem('refreshToken', newRefreshToken);
@@ -90,29 +64,16 @@ export default createStore({
     async signInViaCredentials(context, { username, password }) {
       if (!username) throw new Error('Invalid argument. Username is falsy.');
       if (!password) throw new Error('Invalid arguments. Password is falsy.');
+      const { accessToken, refreshToken } = requestTokens({ username, password });
+      const accessTokenPayload = parseJwt(accessToken);
 
-      try {
-        const response = await axios.post(`${ process.env.VUE_APP_API_ENDPOINT }/auth/signin`, { username, password, });
-        const payload = parseJwt(response.data.accessToken);
+      localStorage.setItem('accessToken', accessToken);
+      localStorage.setItem('refreshToken', refreshToken);
 
-        localStorage.setItem('accessToken', response.data.accessToken);
-        localStorage.setItem('refreshToken', response.data.refreshToken); // TODO https://medium.com/@sadnub/simple-and-secure-api-authentication-for-spas-e46bcea592ad
+      context.commit('setUsername', accessTokenPayload.username);
+      context.commit('setIsLoggedIn', true);
 
-        context.commit('setUsername', payload.username);
-        context.commit('setIsLoggedIn', true);
-
-        axios.defaults.headers.common['Authorization'] = `Bearer ${ localStorage['accessToken'] }`;
-      } catch (error) {
-        if (error && error.response && error.response.data && error.response.data.message) {
-          console.error(error.response.data.message);
-          alert(error.response.data.message);
-        } else {
-          console.error(error);
-          alert('Error: Could not sign in user');
-        }
-
-        throw new Error('Could not sign in user');
-      }
+      axios.defaults.headers.common['Authorization'] = `Bearer ${ localStorage['accessToken'] }`;
     },
     logOut(context) {
       localStorage.removeItem('accessToken');
