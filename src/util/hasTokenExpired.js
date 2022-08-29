f2e625f268cdd37ee76667a611f8e7c3e2e1d557
Smail <44433524+Smail@users.noreply.github.com>
2022-08-29 15:08:09 +0200

    fix(jwt): you had one job...

--- src/util/hasTokenExpired.js
+++ src/util/hasTokenExpired.js
@@ -1,3 +1,5 @@
+import { parseJwt } from "@/util/parseJwt";
+
 export function hasTokenExpired(token) {
-  return Date.now() < token.exp * 1000;
+  return Date.now() > parseJwt(token).exp * 1000;
 }
