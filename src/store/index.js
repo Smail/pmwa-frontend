ffac366cf2f55429be65ecf41683cc990a36cf67
Smail <44433524+Smail@users.noreply.github.com>
2022-09-09 17:34:05 +0200

    feat: Add action and mutation updateUser, which updates all new keys at once

--- src/store/index.js
+++ src/store/index.js
@@ -36,12 +36,53 @@ export default createStore({
         state.user[key] = user[key];
       }
     },
+    updateUser: (state, user) => {
+      const updatableKeys = Object.keys(state.user);
+
+      // Check if all keys contained in the new user object actually exist on our current one,
+      // i.e., no additional unknown keys are added to the state user object.
+      for (const key of Object.keys(user)) {
+        if (!updatableKeys.includes(key)) throw new Error(`Unknown key on user object '${ key }'`);
+      }
+
+      for (const key of updatableKeys) {
+        state.user[key] = user[key];
+      }
+    },
     setIsLoggedIn: (state, isLoggedIn) => state.isLoggedIn = isLoggedIn,
     toggleShowClock: (state) => state.showClock = !state.showClock,                      // TODO update on server
     toggleShowClockSeconds: (state) => state.showClockSeconds = !state.showClockSeconds, // TODO update on server
     setTasks: (state, tasks) => state.tasks = tasks,
   },
   actions: {
+    async updateUser(context, newUser) {
+      const currentUser = context.state.user;
+
+      // Check if all keys contained in the new user object actually exist on the current one,
+      // i.e., no additional (unknown) keys are added to the state user object.
+      const updatableKeys = Object.keys(currentUser);
+      for (const key of Object.keys(newUser)) {
+        if (!updatableKeys.includes(key)) throw new Error(`Unknown key on user object '${ key }'`);
+      }
+
+      // Force the username to be lowercase
+      if (!newUser.username) newUser.username = newUser.username.toLowerCase();
+
+      // Contains only the keys that have changed
+      const changedUserObject = {};
+      for (const key of Object.keys(newUser)) {
+        if (currentUser[key] !== newUser[key]) changedUserObject[key] = newUser[key];
+      }
+
+      if (Object.keys(changedUserObject).length > 0) {
+        const response = await axios.patch(`users/${ currentUser.username }`, changedUserObject);
+        // Update user object locally, i.e., client-side
+        context.commit("updateUser", changedUserObject);
+        console.debug("Successfully updated the user");
+      } else {
+        console.debug("User object has not changed. No updates required");
+      }
+    },
     /**
      * Sign the user in using either user-supplied credentials or previously requested tokens.
      *
