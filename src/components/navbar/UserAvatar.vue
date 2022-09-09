64ffe4e42a0891c126b32f39dad86d60b8139c78
Smail <44433524+Smail@users.noreply.github.com>
2022-09-09 16:10:56 +0200

    fix: Get display name with the new user store object

--- src/components/navbar/UserAvatar.vue
+++ src/components/navbar/UserAvatar.vue
@@ -5,9 +5,9 @@
     <span v-else class="material-symbols-outlined user-avatar-icon">account_circle</span>
     <div class="user-avatar-greeting">
       <p>Good Day 👋</p>
-      <h2 v-if="displayName">{{ displayName }}</h2>
-      <h2 v-else-if="$store.state.username">{{ $store.state.username }}</h2>
-      <h2 v-else>Error: User is logged in, but username is not defined.</h2>
+      <h2 v-if="$store.state.user.displayName">{{ $store.state.user.displayName }}</h2>
+      <h2 v-else-if="$store.state.user.username">{{ $store.state.user.username }}</h2>
+      <h2 v-else>Error: User is logged in, but no username defined.</h2>
     </div>
   </div>
 </template>
@@ -46,24 +46,7 @@
 </style>
 
 <script>
-import axios from "axios";
-
 export default {
   name: "UserAvatar",
-  methods: {
-    async requestDisplayName() {
-      const response = axios.get(`users/${ this.$store.state.username }/display-name`);
-      return (await response).data.displayName;
-    },
-  },
-  data() {
-    return {
-      displayName: this.$store.state.username,
-    };
-  },
-  async created() {
-    const displayName = await this.requestDisplayName();
-    if (displayName) this.displayName = displayName;
-  },
 };
 </script>
