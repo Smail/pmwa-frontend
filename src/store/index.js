b2cf86f230e5a2b3143bb97a903bd41599055f28
Smail <44433524+Smail@users.noreply.github.com>
2022-09-09 15:59:06 +0200

    fix: User authentication and user data request

--- src/store/index.js
+++ src/store/index.js
@@ -8,7 +8,14 @@ import { parseJwt } from "@/util/parseJwt";
 
 export default createStore({
   state: {
-    username: "",
+    user: {
+      userId: null,
+      username: null,
+      email: null,
+      displayName: null,
+      firstName: null,
+      lastName: null,
+    },
     isLoggedIn: false,
     locale: navigator.language, // TODO add to backend (and jwt)
     showClock: true,
@@ -17,7 +24,18 @@ export default createStore({
   },
   getters: {},
   mutations: {
-    setUsername: (state, username) => state.username = username,
+    setUser: (state, user) => {
+      const requiredKeys = Object.keys(state.user);
+
+      // To prevent inconsistent state, we first check if all keys exists and then assign the values.
+      for (const key of requiredKeys) {
+        if (!user.hasOwnProperty(key) || user[key] == null) throw new Error(`Missing key '${ key }'`);
+      }
+
+      for (const key of requiredKeys) {
+        state.user[key] = user[key];
+      }
+    },
     setIsLoggedIn: (state, isLoggedIn) => state.isLoggedIn = isLoggedIn,
     toggleShowClock: (state) => state.showClock = !state.showClock,                      // TODO update on server
     toggleShowClockSeconds: (state) => state.showClockSeconds = !state.showClockSeconds, // TODO update on server
@@ -49,13 +67,23 @@ export default createStore({
       axios.defaults.headers.common["Authorization"] = `Bearer ${ tokens.accessToken }`;
 
       const accessTokenPayload = parseJwt(tokens.accessToken);
-      context.commit("setUsername", accessTokenPayload.username);
+      await context.dispatch("requestUserData", accessTokenPayload.userId);
+
       context.commit("setIsLoggedIn", true);
     },
+    async requestUserData(context, userId) {
+      axios.get(`/users/${ userId }`).then(response => context.commit("setUser", response.data));
+    },
     logOut(context) {
+      // Delete tokens
       localStorage.removeItem("accessToken");
       localStorage.removeItem("refreshToken");
-      context.commit("setUsername", "");
+
+      // Reset all user object values to null
+      for (const key of Object.keys(state.user)) {
+        state.user[key] = null;
+      }
+
       context.commit("setIsLoggedIn", false);
     },
     async loadTasks(context) {
