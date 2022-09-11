1c4f07a2723d71cbf9b9e694e06b7b21f0323a6a
Smail <44433524+Smail@users.noreply.github.com>
2022-09-12 00:15:03 +0200

    fix: Make CSS scoped. Class .task exists in multiple namespaces

--- src/components/calendar/Calendar.vue
+++ src/components/calendar/Calendar.vue
@@ -155,7 +155,7 @@ export default {
 };
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 .day-header {
   display: flex;
   align-items: center;
--- src/components/tasks/Task.vue
+++ src/components/tasks/Task.vue
@@ -9,7 +9,7 @@
   </div>
 </template>
 
-<style lang="scss">
+<style lang="scss" scoped>
 .task {
   display: flex;
   gap: 0.5rem;
