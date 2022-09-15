1fa7c6148794b5bda74fafcbd8e55c2174642c84
Smail <44433524+Smail@users.noreply.github.com>
2022-09-15 22:16:09 +0200

    feat: Export functions requestTokensViaRefreshToken, requestTokensViaCredentials

--- src/services/requestNewTokens.js
+++ src/services/requestNewTokens.js
@@ -19,7 +19,7 @@ export async function requestNewTokens({ refreshToken, username, password }) {
   }
 }
 
-async function requestTokensViaRefreshToken(refreshToken) {
+export async function requestTokensViaRefreshToken(refreshToken) {
   if (refreshToken == null) throw new Error("No refresh token found in local storage");
   if (hasTokenExpired(refreshToken)) throw new Error("Refresh token has expired");
   const refreshTokenPayload = parseJwt(refreshToken);
@@ -31,7 +31,7 @@ async function requestTokensViaRefreshToken(refreshToken) {
   return { accessToken: response.data.accessToken, refreshToken: response.data.refreshToken };
 }
 
-async function requestTokensViaCredentials(username, password) {
+export async function requestTokensViaCredentials(username, password) {
   if (!username) throw new Error("Invalid argument. Username is falsy.");
   if (!password) throw new Error("Invalid arguments. Password is falsy.");
 
