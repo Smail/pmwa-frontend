36a7d3e099a38d7dea1d9449aefbd4c24fd2cee4
Smail <44433524+Smail@users.noreply.github.com>
2022-09-15 23:33:05 +0200

    feat(tasks): Add optional task parameter ID to route

--- src/router/index.js
+++ src/router/index.js
@@ -34,7 +34,7 @@ const routes = [
     component: () => import("../views/CalendarView.vue"),
   },
   {
-    path: "/tasks",
+    path: "/tasks/:id?",
     name: "tasks",
     alias: "/todo",
     meta: { needsAuthentication: true },
