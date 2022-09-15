b367b02c68a494e84511b98e3bf128dfcacb8fe0
Smail <44433524+Smail@users.noreply.github.com>
2022-09-15 22:08:30 +0200

    refactor: Check for null and make error more expressive

--- src/util/parseJwt.js
+++ src/util/parseJwt.js
@@ -1,5 +1,5 @@
 export function parseJwt(token) {
-  if (!token) throw new Error("Invalid argument");
+  if (token == null) throw new Error("Invalid argument: token is null");
   const base64Url = token.split(".")[1];
   const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
   const jsonPayload = decodeURIComponent(window.atob(base64).split("").map(function (c) {
