91d5f8734a70336e1df2c423cdd57a010d42140c
Smail <44433524+Smail@users.noreply.github.com>
2022-09-16 21:07:57 +0200

    refactor: requestUserData doesn't do what it says it does. It now returns the requested user data instead of setting it to the state user object

--- src/store/index.js
+++ src/store/index.js
@@ -173,7 +173,7 @@ export default createStore({
       // TODO fix: currently user id is passed and somehow it works when an auth header is set
       if (username == null) throw new Error("Invalid argument: username is null");
       try {
-        context.commit("setUser", (await axios.get(`/users/${ username }`)).data);
+        return (await axios.get(`/users/${ username }`)).data;
       } catch (e) {
         console.error("Could not get user data: %s", e.message);
         throw new Error("Could not get user data", { cause: e });
