9ba626b7baa30243c60d2befced350bb5bd62ee7
Smail <44433524+Smail@users.noreply.github.com>
2022-09-17 01:41:15 +0200

    refactor!: Check for invalid arguments in setTokens

--- src/services/setTokens.js
+++ src/services/setTokens.js
@@ -1,4 +1,12 @@
+import { parseJwt } from "@/util/parseJwt";
+
 export function setTokens({ accessToken, refreshToken }) {
+  if (accessToken == null) throw new Error("setTokens: Access token is null");
+  if (refreshToken == null) throw new Error("setTokens: Refresh token is null");
+  //  TODO jwt verify; Check if valid tokens
+  parseJwt(accessToken);
+  parseJwt(refreshToken)
+
   localStorage.setItem("accessToken", accessToken);
   localStorage.setItem("refreshToken", refreshToken);
 }
