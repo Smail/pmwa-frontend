5ad792f91ec107bd0a7e8b99950c02e98309dd99
Smail <44433524+Smail@users.noreply.github.com>
2022-09-18 11:32:05 +0200

    refactor: Remove commented out code

--- src/components/navbar/NavBarLoggedIn.vue
+++ src/components/navbar/NavBarLoggedIn.vue
@@ -11,20 +11,6 @@
       <li v-for="menuItem in menu" class="menu">
         <nav-bar-link :icon="menuItem.icon" :name="menuItem.name" :sub-menu="menuItem.subMenu" :to="menuItem.routeName"
                       class="wrapper"></nav-bar-link>
-
-        <!--        <router-link :to="menuItem.href" class="menu-link">-->
-        <!--          <div class="menu-title">-->
-        <!--            <span v-if="menuItem.icon" class="material-symbols-outlined">{{ menuItem.icon }}</span>-->
-        <!--            {{ menuItem.name }}-->
-        <!--          </div>-->
-        <!--          <ul v-show="menuItem.subMenu && menuItem.subMenu.length > 0" class="sub-menu-list">-->
-        <!--            <li v-for="subMenu in menuItem.subMenu">-->
-        <!--              <router-link :to="`${menuItem.href}/filter/tags/${subMenu}`" class="menu-link">-->
-        <!--                {{ subMenu }}-->
-        <!--              </router-link>-->
-        <!--            </li>-->
-        <!--          </ul>-->
-        <!--        </router-link>-->
       </li>
 
       <!-- Log out button -->
@@ -59,19 +45,6 @@ export default {
         { name: "Settings", href: "/settings", icon: "settings", routeName: "settings" },
       ],
     };
-
-    // Load tasks submenu
-    // axios.get('tasks/tags/names').then(response => {
-    //   for (const tagName of response.data) {
-    //     this.menu[1].subMenu.push({
-    //       name: tagName,
-    //       icon: 'sell',
-    //       to: 'filter'
-    //     });
-    //   }
-    //   console.log(data.menu[1])
-    // });
-
     return data;
   },
 };
