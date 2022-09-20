b5e0e30a9826e8b7aa6538ccc3bc551edf281ba3
Smail <44433524+Smail@users.noreply.github.com>
2022-09-21 01:51:07 +0200

    feat: Create sidebar for log in view

---
+++ src/components/navbar/SidebarItem.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="sidebar-item">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SidebarItem",
+}
+</script>
+
+<style lang="scss">
+.sidebar-item {
+  display: flex;
+  align-items: center;
+  gap: 0.5em;
+  padding: 0.5em;
+  border-radius: 0.5em;
+  background: var(--primary-color-500);
+}
+</style>
---
+++ src/components/navbar/SidebarLink.vue
@@ -0,0 +1,63 @@
+<template>
+  <sidebar-item class="sidebar-item">
+    <router-link :to="{ name: routeName }" class="sidebar-link">
+      <span class="material-symbols-outlined">{{ icon }}</span>
+      <span class="name">{{ name }}</span>
+    </router-link>
+  </sidebar-item>
+</template>
+
+<script>
+import SidebarItem from "@/components/navbar/SidebarItem";
+
+export default {
+  name: "SidebarLink",
+  components: { SidebarItem },
+  props: {
+    routeName: {
+      type: String,
+      require: true,
+    },
+    name: {
+      type: String,
+      require: true,
+    },
+    icon: {
+      type: String,
+      require: true,
+    },
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.sidebar-item {
+  padding: 0;
+  background-color: transparent;
+}
+
+.sidebar-link {
+  flex: 1;
+  display: flex;
+  align-items: center;
+  padding: 0.5em;
+  gap: inherit;
+  border-radius: inherit;
+
+  &.router-link-active {
+    color: white;
+    box-shadow: 0 0 0.5em var(--primary-color-500);
+    background: var(--primary-color-500);
+  }
+
+  &:hover {
+    color: white;
+    box-shadow: 0 0 0.5em var(--primary-color-600);
+    background: var(--primary-color-600);
+  }
+
+  &:active {
+    background: var(--primary-color-400);
+  }
+}
+</style>
