df48f38e6d016af3cb74bb1b1e7618450a87c61e
Smail <44433524+Smail@users.noreply.github.com>
2022-10-05 16:22:30 +0200

    fix: Make credentials only valid if they're also not of length 0

--- src/services/areCredentialsValid.js
+++ src/services/areCredentialsValid.js
@@ -1,3 +1,4 @@
 export function areCredentialsValid(credentials) {
-  return credentials != null && credentials.username != null && credentials.password != null;
+  return credentials != null && credentials.username != null && credentials.password != null
+    && credentials.username.length > 0 && credentials.password.length > 0;
 }
